3724663e6441c5f1c479df49c2853863bbaaa15b
Ilsakhan123 <154370842+Ilsakhan123@users.noreply.github.com>
2023-12-27 00:34:49 +0500

    Update Part1 (cs).cpp
    Board Display

--- Part1 (cs).cpp
+++ Part1 (cs).cpp
@@ -15,3 +15,22 @@ void Game_play(string a, string b);
 bool Check_Game();
 bool Check_Draw();
 void End_Game(string a, string b);
+void Display_board(void)
+{
+    system("CLS");
+
+    cout<<endl<<endl<<endl<<"\t\t\t\t\t                    TABLE-CLASH GAME                    "<<endl<<endl<<endl<<endl;
+    cout << endl << endl;
+    cout<<"\t\t\t\t\t Status : "<<status;
+    cout << endl << endl;
+
+    cout << "\t\t\t\t\t\t\t     |      |       " << endl;
+    cout << "\t\t\t\t\t\t\t  " << display[0][0] << "  |  " << display[0][1] << "   |  " << display[0][2] << endl;
+    cout << "\t\t\t\t\t\t\t_____|______|_______" << endl;
+    cout << "\t\t\t\t\t\t\t     |      |       " << endl;
+    cout << "\t\t\t\t\t\t\t  " << display[1][0] << "  |  " << display[1][1] << "   |  " << display[1][2] << endl;
+    cout << "\t\t\t\t\t\t\t_____|______|_______" << endl;
+    cout << "\t\t\t\t\t\t\t     |      |       " << endl;
+    cout << "\t\t\t\t\t\t\t  " << display[2][0] << "  |  " << display[2][1] << "   |  " << display[2][2] << endl;
+    cout << "\t\t\t\t\t\t\t     |      |       " << endl;
+}
